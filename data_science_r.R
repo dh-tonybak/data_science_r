37e01b26a2b7d8f29524031c1fa618ebafccf302
tony <dh.tonybak@gmail.com>
2021-04-26 23:36:52 +0900

    practice

--- data_science_r.R
+++ data_science_r.R
@@ -1,6 +1,9 @@
 boston <- read.table("C:/Projects/data_science_r/housing.data")
 
+install.packages("gapminder")
+
 library(dplyr)
+library(gapminder)
 glimpse(boston)
 
 # 변수명 할당
@@ -15,3 +18,29 @@ summary(boston)
 # 확인 결과 github에서 파일 삭제하였지만 local에는 파일이 남아있어서 생기는 오류
 # pull을 사용하여 해결
 
+# dplyr tutorial
+i2 <- tbl_df(iris)
+class(i2)
+i2
+glimpse(i2)
+
+iris %>% head
+
+#filter
+filter(gapminder, country == 'Korea, Rep.')
+filter(gapminder, year == 2007)
+filter(gapminder, country == 'Korea, Rep.' & year == 2007)
+
+gapminder %>% filter(country == 'Korea, Rep.')
+gapminder %>% filter(year == 2007)
+gapminder %>% filter(country == 'Korea, Rep.' & year == 2007)
+
+#arrange
+arrange(gapminder, year, country)
+gapminder %>% arrange(year, country)
+
+#select
+select(gapminder, pop, gdpPercap)
+gapminder %>% select(pop, gdpPercap)
+
+#mutate
